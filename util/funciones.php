5ada0909e78b6aff615aae309fef654aacc16451
Josemi <worldcode.info@gmail.com>
2024-04-18 12:28:05 +0200

    Crea la vista de música

--- util/funciones.php
+++ util/funciones.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__."/../config/app.php";
 
+use App\config\Config;
 use App\helpers\Mp3InfoHelper;
 use Model\ActiveRecord;
 use wapmorgan\Mp3Info\Mp3Info;
@@ -31,6 +32,43 @@ function debuguearSinExit($variable) {
     var_dump($variable);
     echo "</pre>";
 }
+
+/**
+ * Función que devuelve la ruta de una imagen diferente cada día del año.
+ * La imagen se va rotando en función de la cantidad de imágenes que haya
+ *
+ * @param string $type
+ * @param integer $offset
+ * @return string
+ */
+function imageOfTheDay($type = 'jpg', $offset = 0): string {
+	$offset = getIndexOfScript();
+	if(!in_array($type, Config::IMAGE_TYPES)) $type = 'jpg';
+	$dir = dir(Config::ABSOLUTE_DIR_IMG_PRINCIPAL);
+	$images = [];
+	while (false !== $entry = $dir->read()) {
+		$img = explode('.', $entry);
+		if(end($img) === $type){
+			$images[] = Config::DIR_IMG_PRINCIPAL . $entry;
+		}
+	}
+	$max = count($images);
+	$day = (int) date('z');
+	$index = ($day + $offset) % $max;
+	return $images[$index];
+}
+
+function getIndexOfScript() {
+	$offset = 0;
+	$script = basename($_SERVER['SCRIPT_FILENAME']);
+	$index = array_search($script, Config::PAGES);
+	if(is_int($index)){
+		$offset = $index;
+	}
+	return $offset;
+}
+
+
 
 /**
  * Función que recibe un nombre compuesto por palabras separadas por
--- views/index/musica.php
+++ views/index/musica.php
@@ -2,6 +2,6 @@
 require DIR_ROOT.'/templates/header.php';
 ?>
 
-    <h1>Lista de programas</h1>
+    <h1>Lista de música</h1>
     <?php require DIR_ROOT."/templates/datatablespru.php"; ?>
 <?php require DIR_ROOT.'/templates/footer.php' ?>
